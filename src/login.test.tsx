8626c8bbe9ac74f72b432fb9d5fd12f087b3c0ee
Paul Rieutord <prieutord@kunder.cl>
2020-12-11 11:57:26 -0300

    added tests (part 35)

---
+++ src/login.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { fireEvent, queryByText } from '@testing-library/react';
+
+import { Login } from './login';
+import { LoginService } from './services/LoginService';
+
+describe('Login component tests', () => {
+
+  let container: HTMLDivElement
+  const loginServiceSpy = jest.spyOn(LoginService.prototype, 'login');
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    ReactDOM.render(<Login />, container);
+  })
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container.remove();
+  })
+
+  it('Renders correctly initial document', () => {
+    const inputs = container.querySelectorAll('input');
+    expect(inputs).toHaveLength(3);
+    expect(inputs[0].name).toBe('login');
+    expect(inputs[1].name).toBe('password');
+    expect(inputs[2].value).toBe('Login');
+
+    const label = container.querySelector('label');
+    expect(label).not.toBeInTheDocument();
+
+  });
+
+  it('Renders correctly initial document with data-test query', () => {
+    expect(container.querySelector("[data-test='login-form']")).toBeInTheDocument();
+    expect(container.querySelector("[data-test='login-input']")?.getAttribute('name'))
+      .toBe('login');
+    expect(container.querySelector("[data-test='password-input']")?.getAttribute('name'))
+      .toBe('password');
+  });
+}) 
